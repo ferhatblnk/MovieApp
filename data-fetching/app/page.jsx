5a3caa7c455eaa6d783c8bdfaa8907ef43d18d0b
Ferhat Talha BULANIK <ferhattalhabulanik@Ferhats-MacBook-Pro.local>
2023-09-26 13:12:40 +0300

    Cache With Timer

--- data-fetching/app/page.jsx
+++ data-fetching/app/page.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    cache : 'no-store',
+    // next : {revalidate : 10} Cache with time
+  });
   return res.json();
 };
 
