9e82e8b289812e9fd8a1987581941620008be969
Ferhat Talha BULANIK <ferhattalhabulanik@Ferhats-MacBook-Pro.local>
2023-09-26 13:17:14 +0300

    Promise All Process

--- data-fetching/app/page.jsx
+++ data-fetching/app/page.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 
-const getData = async () => {
+const getData1 = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-    cache : 'no-store',
+    cache: "no-store",
+    // next : {revalidate : 10} Cache with time
+  });
+  return res.json();
+};
+
+const getData2 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/2", {
+    cache: "no-store",
+    // next : {revalidate : 10} Cache with time
+  });
+  return res.json();
+};
+
+const getData3 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/3", {
+    cache: "no-store",
     // next : {revalidate : 10} Cache with time
   });
   return res.json();
 };
 
 async function HomePage() {
-  const data = await getData();
-  console.log(data);
+  const [getData1, getData2, getData3] = await Promise.all([
+    data1,
+    data2,
+    data3,
+  ]);
+
+  console.log(data1,data2,data3);
+  // const data = await getData();
+  // console.log(data);
   return <h1>Data fetching</h1>;
 }
 
